aa4c56e122ecd481ab299b7be74349663f5b930f
ksd101jp14 <46618187+kd101jp14@users.noreply.github.com>
2019-05-08 23:31:46 -0500

    Created authentication with passport.js for application.

---
+++ models/user.js
@@ -0,0 +1,57 @@
+module.exports = function(sequelize, Sequelize) {
+  var User = sequelize.define("user", {
+    id: {
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+
+    firstName: {
+      type: Sequelize.STRING,
+      notEmpty: true,
+      allowNull: false,
+      len: [2, 30]
+    },
+
+    lastName: {
+      type: Sequelize.STRING,
+      notEmpty: true,
+      allowNull: false,
+      len: [2, 30]
+    },
+
+    username: {
+      type: Sequelize.TEXT,
+      notEmpty: true,
+      allowNull: false,
+      len: [2, 30]
+    },
+
+    email: {
+      type: Sequelize.STRING,
+      notEmpty: true,
+      allowNull: false,
+      validate: {
+        isEmail: true
+      }
+    },
+
+    password: {
+      type: Sequelize.STRING,
+      notEmpty: true,
+      allowNull: false,
+      len: [6, 18]
+    },
+
+    lastLogin: {
+      type: Sequelize.DATE
+    },
+
+    status: {
+      type: Sequelize.ENUM("active", "inactive"),
+      defaultValue: "active"
+    }
+  });
+
+  return User;
+};
