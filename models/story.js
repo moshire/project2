25ea8073edef4c0f76e8037a0f0b7022cc259596
ksd101jp14 <46618187+kd101jp14@users.noreply.github.com>
2019-05-10 20:12:29 -0500

    Updated 'story' table to change 'summary' column to story.

--- models/story.js
+++ models/story.js
@@ -7,23 +7,25 @@ var Sequelize = require("sequelize");
 var sequelize = require("../config/connection");
 
 // Creates a "story" model that matches up with DB
-var story = sequelize.define("story", {
-  // the routeName gets saved as a string
-  routeName: Sequelize.STRING,
-  // the name of the user (a string)
-  name: Sequelize.STRING,
-  // the story's title (a string)
-  title: Sequelize.STRING,
-  // the story's summary (a string)
-  summary: Sequelize.STRING,
-  
-  
-}, {
-  // disable the modification of tablenames; By default, sequelize will automatically
-  // transform all passed model names (first parameter of define) into plural.
-  // if you don't want that, set the following
-  freezeTableName: true
-});
+var story = sequelize.define(
+  "story",
+  {
+    // the routeName gets saved as a string
+    routeName: Sequelize.STRING,
+    // the name of the user (a string)
+    name: Sequelize.STRING,
+    // the story's title (a string)
+    title: Sequelize.STRING,
+    // the story's summary (a string)
+    story: Sequelize.STRING
+  },
+  {
+    // disable the modification of tablenames; By default, sequelize will automatically
+    // transform all passed model names (first parameter of define) into plural.
+    // if you don't want that, set the following
+    freezeTableName: true
+  }
+);
 
 // Syncs with DB
 story.sync();
