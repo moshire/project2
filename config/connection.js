016565d23ca013d7a552841e6a3a02d67a92b30c
mohamed shire <m.x.shire876@hotmail.com>
2019-05-19 20:25:15 -0500

    make it better

--- config/connection.js
+++ config/connection.js
@@ -9,25 +9,26 @@ var connection;
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  // connection = mysql.createConnection({
-  //   host: "localhost",
-  //   user: "root",
-  //   password: "somali4047669",
-  //   database: "sequelize_passport"
-  // })
-
-  // Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
-  var sequelize = new Sequelize("sequelize_passport", "root", "somali4047669", {
+  connection = mysql.createConnection({
     host: "localhost",
-    port: 3306,
-    dialect: "mysql",
-    pool: {
-      max: 5,
-      min: 0,
-      idle: 10000
-    }
+    user: "root",
+    password: "somali4047669",
+    database: "sequelize_passport"
   });
 }
+
+// Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
+var sequelize = new Sequelize("sequelize_passport", "root", "somali4047669", {
+  host: "localhost",
+  port: 3306,
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  }
+});
+
 // Exports the connection for other files to use
 module.exports = sequelize;
 module.exports = connection;
