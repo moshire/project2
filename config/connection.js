f79c9ea94922eeeca650e28c2cfd529ac7fb7870
mohamed shire <m.x.shire876@hotmail.com>
2019-05-19 21:10:30 -0500

    make it better

--- config/connection.js
+++ config/connection.js
@@ -3,32 +3,32 @@
 // *********************************************************************************
 
 // Dependencies
-var Sequelize = require("sequelize");
-var mysql = require("mysql");
-var connection;
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "somali4047669",
-    database: "sequelize_passport"
-  });
-}
+// var Sequelize = require("sequelize");
+// var mysql = require("mysql");
+// var connection;
+// if (process.env.JAWSDB_URL) {
+//   connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+//   connection = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "somali4047669",
+//     database: "sequelize_passport"
+//   });
+// }
 
-// Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
-var sequelize = new Sequelize("sequelize_passport", "root", "somali4047669", {
-  host: "localhost",
-  port: 3306,
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  }
-});
+// // Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
+// var sequelize = new Sequelize("sequelize_passport", "root", "somali4047669", {
+//   host: "localhost",
+//   port: 3306,
+//   dialect: "mysql",
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     idle: 10000
+//   }
+// });
 
-// Exports the connection for other files to use
-module.exports = sequelize;
-module.exports = connection;
+// // Exports the connection for other files to use
+// module.exports = sequelize;
+// module.exports = connection;
